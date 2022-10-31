5f9cfa8abfb1bdc1294027bfba5436e29d135f6b
Ravi Kumar <ravikmar601@gmail.com>
2022-10-31 15:17:49 +0530

    added state in tictactoe

--- src/js/Tic-Tac-Toe.js
+++ src/js/Tic-Tac-Toe.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 
+import Board from "./Board";
+
 export default class TicTacToe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      isXturn: true,
+      history: [],
+    };
+  }
+  handleMove(index) {}
+  moveToHistory(index) {}
   render() {
-    return <>TicTacToe</>;
+    return (
+      <div className="TicTacToe">
+        <Board
+          squares={this.state.squares}
+          isXturn={this.state.isXturn}
+          onClick={this.handleMove}
+        />
+      </div>
+    );
   }
 }
